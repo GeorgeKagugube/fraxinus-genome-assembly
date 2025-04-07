69cec34add1fe082904afe35d43e9fd59db4cda2
GeorgeKagugube <ggrwilliam@yahoo.co.uk>
2025-04-07 16:40:43 +0100

    modified busco analysis to use conda environmnet

--- scripts/busco.sh
+++ scripts/busco.sh
@@ -13,10 +13,9 @@
 #$ -wd /lustre/scratch/scratch/uczrgwk/manchurica_ash/scripts/assemblies/busco_analysis 
 
 ## activate the required conda environments here 
-#conda activate /home/uczrgwk/miniconda3/envs/busco
+conda activate /home/uczrgwk/miniconda3/envs/busco
 
 assembly="/home/uczrgwk/Scratch/manchurica_ash/assemblies/flye_assembly/assembly.fasta"
 
 ## Run busco here
-/home/uczrgwk/Scratch/manchurica_ash/software/busco-4.0.1/bin/busco -i $assembly -o qc_analysis -l eukaryota_odb10 -m geno -f --scaffold_composition
-/home/uczrgwk/Scratch/manchurica_ash/software/busco-4.0.1/bin/busco -i $assembly -o qc_analysis -l eudicots_odb10 -m geno -f --scaffold_composition
+busco -i $assembly -o qc_analysis -l eukaryota_odb10 -m geno -f --scaffold_composition
